f58dbe21fb347796777ad72d9997925519c999af
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-06 13:10:17 +0000

    chore(deps): Updated

--- src/cluster/index.ts
+++ src/cluster/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hcs/0.5.1/docs/resources/cluster
 // generated from terraform resource schema
 
